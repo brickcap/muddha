3120e6c889635f5737b88de8474b4818ec6bcc14
brickcap <akshatjiwan@gmail.com>
2017-04-27 16:13:56 +0530

    Penlight loads everything into a global spcae

--- app/server/load_packages.lua
+++ app/server/load_packages.lua
@@ -1,15 +1,17 @@
---load init.lua and .lua files in the lib directory
+
 package.path = package.path .. ";lib/?.lua"
 package.path = package.path .. ";lib/init.lua"
 
---load all the compiled lua files in the lib directory
+
 package.cpath = package.cpath .. ";lib/?.so"
 
--- load all the init.lua files in a subdirectory inside lib directory
+
 package.path = package.path .. ";lib/?/init.lua"
 package.path = package.path .. ";lib/?/?.lua"
 
-print(package.path)
-print(package.searchpath('turbo',package.path))
+package.path = package.path .. ";"
 
-return "ok"
+print(package.path)
+print(package.searchpath('pl.pretty',package.path))
+
+return true
--- app/server/test-server.lua
+++ app/server/test-server.lua
@@ -1,18 +1,19 @@
-package.path = package.path .. ";lib/?/?.lua"
-package.path = package.path .. ";lib/?.lua"
-
-local lfs = require("lib/-lfs")
-local lcurl = require("lib/-lcurl")
+local l_packages = require("load_packages")
+local lfs = require("-lfs")
+require("pl")
+local lcurl = require("-lcurl")
 local turbo = require("turbo")
-local s = require "lib/-lsqlite3complete"
-local db = s.open("lib/test.lite")
+local s = require "-lsqlite3complete"
+local db = s.open("data/test.lite")
 local res = db:load_extension("lib/json1")
+print(type(pl))
+print(pretty.write({hello="what",my="name is anthony"}))
 print(res)
 print(s:version())
 -- Create a new requesthandler with a method get() for HTTP GET.
 local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
 function HelloWorldHandler:get()
-   self:write("Hello World!")
+   self:write({what="Hello World!"})
 end
 
 -- Create an Application object and bind our HelloWorldHandler to the route '/hello'.
