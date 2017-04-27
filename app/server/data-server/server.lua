34e5c1541fdcc0e6659c706eae3d6ec59564824e
brickcap <akshatjiwan@gmail.com>
2017-04-27 19:32:53 +0530

    defined a few database types and the extensions to be used on them

--- app/server/data-server/server.lua
+++ app/server/data-server/server.lua
@@ -1,6 +1,33 @@
 local l_packages = require("load_packages")
+local sqlite = require("-lsqlite3complete")
 local turbo = require("turbo")
 -- Create a new requesthandler with a method get() for HTTP GET.
+
+local get_db = function(db_type,db_name)
+   -- creates a new database or
+   -- returns a handler to an existing database
+   
+   local db = sqlite:open("data/"..db_name)
+   -- crete a full text search virtual table
+   -- on the databases
+   
+   if db_type == "user" then
+      -- encrypt this database with an sqlite extention
+      -- index the table on the email field so that it may
+      -- be eaiser to query
+   end
+   if db_type == "booking" then
+     
+   end
+
+   if db_type == "meta" then
+      end
+
+   if db_type == "commerce" then
+   end
+   
+end
+
 local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
 function HelloWorldHandler:get()
    self:write({what="Hello World!"})
