87ee6127226e703e7e702981348289b7e9aec72a
brickcap <akshatjiwan@gmail.com>
2017-04-27 17:30:39 +0530

    paths will work relative to the the location to jit runtime

--- app/server/load_packages.lua
+++ app/server/load_packages.lua
@@ -1,3 +1,5 @@
+-- start lua runtime from the /muddha/app/server directory
+-- to make sure that the relative paths work well
 package.path = package.path .. ";lib/?.lua"
 package.path = package.path .. ";lib/init.lua"
 package.cpath = package.cpath .. ";lib/?.so"
