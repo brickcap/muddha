f354e20e2ea10d2494a9f067bf3ad376988243e1
brickcap <akshatjiwan@gmail.com>
2017-04-27 12:06:25 +0530

    Create app server and data server directories

---
+++ app/server/app-server/test-server.lua
@@ -0,0 +1,25 @@
+package.path = package.path .. ";lib/?/?.lua"
+package.path = package.path .. ";lib/?.lua"
+
+local lfs = require("lib/-lfs")
+local lcurl = require("lib/-lcurl")
+local turbo = require("turbo")
+local s = require "lib/-lsqlite3complete"
+local db = s.open("lib/test.lite")
+local res = db:load_extension("lib/json1")
+print(res)
+print(s:version())
+-- Create a new requesthandler with a method get() for HTTP GET.
+local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
+function HelloWorldHandler:get()
+   self:write("Hello World!")
+end
+
+-- Create an Application object and bind our HelloWorldHandler to the route '/hello'.
+local app = turbo.web.Application:new({
+      {"/hello", HelloWorldHandler}
+})
+
+-- Set the server to listen on port 8888 and start the ioloop.
+app:listen(8888)
+turbo.ioloop.instance():start()
--- app/server/test-server.lua
+++ app/server/test-server.lua
@@ -1,25 +1,23 @@
 package.path = package.path .. ";lib/?/?.lua"
 package.path = package.path .. ";lib/?.lua"
 
- local lfs = require("lib/-lfs")
- local lcurl = require("lib/-lcurl")
-  print(type(lfs.attributes))
-  print(type(lcurl))
- local turbo = require("turbo")
-  local s = require "lib/-lsqlite3complete"
-  local db = s.open("lib/test.lite")
-  local res = db:load_extension("lib/json1")
-  print(res)
-  print(s:version())
+local lfs = require("lib/-lfs")
+local lcurl = require("lib/-lcurl")
+local turbo = require("turbo")
+local s = require "lib/-lsqlite3complete"
+local db = s.open("lib/test.lite")
+local res = db:load_extension("lib/json1")
+print(res)
+print(s:version())
 -- Create a new requesthandler with a method get() for HTTP GET.
 local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
 function HelloWorldHandler:get()
-    self:write("Hello World!")
+   self:write("Hello World!")
 end
 
 -- Create an Application object and bind our HelloWorldHandler to the route '/hello'.
 local app = turbo.web.Application:new({
-    {"/hello", HelloWorldHandler}
+      {"/hello", HelloWorldHandler}
 })
 
 -- Set the server to listen on port 8888 and start the ioloop.
