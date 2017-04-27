775277d398808344a45a602d986d4704cc89a85c
brickcap <akshatjiwan@gmail.com>
2017-04-27 16:33:20 +0530

    no more newlines. removed empty path

--- app/server/load_packages.lua
+++ app/server/load_packages.lua
@@ -1,15 +1,8 @@
-
 package.path = package.path .. ";lib/?.lua"
 package.path = package.path .. ";lib/init.lua"
-
-
 package.cpath = package.cpath .. ";lib/?.so"
-
-
 package.path = package.path .. ";lib/?/init.lua"
 package.path = package.path .. ";lib/?/?.lua"
-
-package.path = package.path .. ";"
 
 
 return true
